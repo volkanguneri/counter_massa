3746a3dd92c0f101b45678bfa4424b39d384a2df
volkanguneri <vocci@hotmail.com>
2024-11-13 17:14:55 +0100

    debugged increment

--- backend/assembly/__tests__/counter-test.spec.ts
+++ backend/assembly/__tests__/counter-test.spec.ts
@@ -1,43 +1,46 @@
-import { Args, stringToBytes, u32ToBytes, bytesToU32} from '@massalabs/as-types';
+import { Args, stringToBytes, u32ToBytes, bytesToU32 } from '@massalabs/as-types';
 import { Storage, setDeployContext, generateEvent } from '@massalabs/massa-as-sdk';
-import { 
-    increment, 
-    initialize, 
-    getCount,
-     constructor } from '../contracts/counter';
+import {
+  increment,
+  initialize,
+  getCount,
+  constructor
+} from '../contracts/counter';
 
 describe('Counter unit tests', () => {
   beforeAll(() => {
     setDeployContext();
-       // init contract
+    // init contract
     constructor();
   });
-  
+
   test('Initial count should be 0', () => {
-    const countStr : string = Storage.get("count");
+    const countStr: string = Storage.get("count");
     expect(countStr).toBe("0");
   });
 
   test('Initialize function should initialize the counter', () => {
     Storage.set("count", "10");
     initialize();
-    const countStr : string = Storage.get("count")
+    const countStr: string = Storage.get("count");
     expect(countStr).toBe("0");
   });
 
   test('getCount function should get the count', () => {
-    Storage.set("count", "10");    
-    const countStr = Storage.get("count"); 
-    const countBytes : StaticArray<u8> = stringToBytes(countStr);
+    Storage.set("count", "10");
+    const countStr: string = Storage.get("count");
+    const countBytes: StaticArray<u8> = stringToBytes(countStr);
     const getCountResult = getCount();
     expect(countBytes).toStrictEqual(getCountResult);
   });
 
-  test('increment', () => {
-    const incrementValueU32 : u32 = 65;
-    const incrementValueBytes : StaticArray<u8> = u32ToBytes(incrementValueU32);
-    const finalCountBytes : StaticArray<u8> = increment(incrementValueBytes); 
-    let finalCountU32 : u32 = new Args(finalCountBytes).nextU32().expect('finalCountBytes argument is missing or invalid');
+  test('increment should increment the count', () => {
+    Storage.set("count", "0");
+    const incrementValueU32: u32 = 65;
+    const incrementValueBytes: StaticArray<u8> = u32ToBytes(incrementValueU32);
+    const finalCountBytes: StaticArray<u8> = increment(incrementValueBytes);
+    const finalCountU32: u32 = new Args(finalCountBytes).nextU32()
+      .expect('finalCountBytes argument is missing or invalid');
     expect(finalCountU32).toStrictEqual(incrementValueU32);
   });
 });
--- backend/assembly/contracts/counter.ts
+++ backend/assembly/contracts/counter.ts
@@ -1,9 +1,9 @@
-import { Storage, Context, generateEvent} from '@massalabs/massa-as-sdk';
+import { Storage, Context, generateEvent } from '@massalabs/massa-as-sdk';
 import { Args, u32ToBytes, stringToBytes } from '@massalabs/as-types';
 
 export function constructor(): void {
-    assert(Context.isDeployingContract(), "Constructor can only be called during deployment");
-    initialize();
+  assert(Context.isDeployingContract(), "Constructor can only be called during deployment");
+  initialize();
 }
 
 // Initializes the counter to 0
@@ -14,20 +14,23 @@ export function initialize(): void {
 
 // Gets the count value
 export function getCount(): StaticArray<u8> {
-  const countStr : string = Storage.get("count");
-  const countBytes : StaticArray<u8> = stringToBytes(countStr);
+  const countStr: string = Storage.get("count");
+  const countBytes: StaticArray<u8> = stringToBytes(countStr);
   return countBytes;
 }
 
-// export function increment(n: StaticArray<u8>): StaticArray<u8> {
-//   // Deserialize the argument as an u32 number
-//   const incrementValue : u32 = new Args(n).nextU32().expect('n argument is missing or invalid');
-//   const countBytes : StaticArray<u8> = getCount();
-  
-//   let countU32 : u32 = new Args(countBytes).nextU32().expect('countBytes argument is missing or invalid');
-//   const newCount : u32 = countU32 += incrementValue;
-//   Storage.set("count", newCount.toString());
-//   generateEvent(`Counter incremented by ${incrementValue}. New value: ${newCount}`);
-//   const newCountBytes : StaticArray<u8> = u32ToBytes(newCount);
-//   return newCountBytes;
-// }
+export function increment(n: StaticArray<u8>): StaticArray<u8> {
+  // Deserialize the argument as an u32 number
+  const incrementValue: u32 = new Args(n).nextU32().expect('n argument is missing or invalid');
+
+  const countStr: string = Storage.get("count");
+
+  let countU32: u32 = u32(i32.parse(countStr));
+  const newCount: u32 = countU32 + incrementValue;
+
+  Storage.set("count", newCount.toString());
+  generateEvent(`Counter incremented by ${incrementValue}. New value: ${newCount}`);
+
+  const newCountBytes: StaticArray<u8> = u32ToBytes(newCount);
+  return newCountBytes;
+}
