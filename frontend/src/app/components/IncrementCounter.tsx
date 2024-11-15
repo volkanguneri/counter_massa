c0d261775d2f6b6117cb4e4eb8900802a781c9fe
volkanguneri <vocci@hotmail.com>
2024-11-15 20:40:27 +0100

    implemented isPending

--- frontend/src/app/components/IncrementCounter.tsx
+++ frontend/src/app/components/IncrementCounter.tsx
@@ -14,9 +14,10 @@ export default function IncrementCounter() {
   const [count, setCount] = useState<bigint>(); // State to dislpay the count from the smart contract
   const [incrementValue, setIncrementValue] = useState<number | "">(""); // State for the input field
   const [account, setAccount] = useState<string>(""); // State for account
-  const [isPendingInc, setIsPendingInc] = useState<boolean>(false); // To create spinner for Increment button 
-  const [isPendingRes, setIsPendingRes] = useState<boolean>(false); // To create spinner for Reset button 
+  const [isPendingInc, setIsPendingInc] = useState<boolean>(false); // To create spinner for Increment button
+  const [isPendingRes, setIsPendingRes] = useState<boolean>(false); // To create spinner for Reset button
   const [events, setEvents] = useState<SCEvent[]>([]);
+  const [eventsStop, setEventsStop] = useState<boolean>(false);
 
   // Inits provider
   const initProvider = useCallback(async () => {
@@ -32,7 +33,7 @@ export default function IncrementCounter() {
 
     // Gets user's account address
     const accounts = await wallet?.accounts();
-    setAccount(accounts[0].address)
+    setAccount(accounts[0].address);
 
     if (accounts.length === 0) {
       console.log("No accounts found");
@@ -42,15 +43,60 @@ export default function IncrementCounter() {
     // We use the first account as the provider
     const provider = accounts[0];
     setProvider(provider);
+
+    //////////////////////////////////////////////////////////
+    // event poller
+
+    // Callback function for handling incoming events
+    const onData = async (events: SCEvent[]) => {
+      setEvents(events);
+      for (const event of events) {
+        console.log(`Event period: ${event.context.slot.period} thread: ${event.context.slot.thread} -`, event.data);
+      }
+    };
+
+    // Callback function for handling errors
+    const onError = (error: Error) => {
+      console.error("Error:", error);
+      setEventsStop(true); // Stop polling in case of an error
+    };
+
+    if (!provider) {
+      console.error("Provider not initialized");
+      return;
+    }
+
+    // Start the event poller with a 5-second interval
+    // const poller = new EventPoller(provider, start, 5000);
+    const { stopPolling } = EventPoller.start(
+      provider,
+      {
+        smartContractAddress: CONTRACT_ADDRESS,
+      },
+      onData,
+      onError,
+      5000 // Polling interval in milliseconds
+    );
+
+    // Continue polling until stopped
+    // while (!stop) {
+    //   await scheduler.wait(5000);
+    // }
+    // stopPolling(); // Stop polling once the loop terminates
   }, []);
 
-  const shortenedAccount = account 
-  ? `${account.slice(0, 4)}...${account.slice(-2)}`
-  : '';
+  const shortenedAccount = account ? `${account.slice(0, 4)}...${account.slice(-2)}` : "";
 
   useEffect(() => {
     initProvider();
   }, [initProvider]);
+
+  useEffect(() => {
+    console.log("useEffect ~ events:", events);
+    if (events.length > 0) {
+      alert(events.at(-1)?.data);
+    }
+  }, [events]);
 
   // Handles wallet connection
   async function connectWallet() {
@@ -84,7 +130,7 @@ export default function IncrementCounter() {
     return resultNumber;
   }
 
-  // Handles input 
+  // Handles input
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const value = Number(e.target.value);
     setIncrementValue(value);
@@ -92,46 +138,23 @@ export default function IncrementCounter() {
 
   // Handles button submit
   const handleSubmit = async (e: React.FormEvent) => {
-    if (!provider) {
-      alert("No provider found");
-      return;
-    }
-    e.preventDefault();
-
-    if (!incrementValue) {
-      alert("Please enter a number");
-      return;
-    }
-
-    setIsPendingInc(true);
-
-    const op = await provider.callSC({
-      parameter: new Args().addU64(BigInt(incrementValue)).serialize(),
-      func: "increment",
-      target: CONTRACT_ADDRESS,
-    });
-
-    const status = await op.waitSpeculativeExecution();
-
-    if (status !== OperationStatus.SpeculativeSuccess) {
-      alert("Failed to set count");
-      return;
-    }
-
-    setCount(await getCount());
-    setIncrementValue("");
-    setIsPendingInc(false);
-  };
-
-    // Handles reset button
-    const handleReset = async ()  => {
+    try {
       if (!provider) {
-        console.log("No provider found");
-        return BigInt(0);
+        alert("No provider found");
+        return;
+      }
+      e.preventDefault();
+
+      if (!incrementValue) {
+        alert("Please enter a number");
+        return;
       }
 
+      setIsPendingInc(true);
+
       const op = await provider.callSC({
-        func: "reset",
+        parameter: new Args().addU64(BigInt(incrementValue)).serialize(),
+        func: "increment",
         target: CONTRACT_ADDRESS,
       });
 
@@ -141,60 +164,46 @@ export default function IncrementCounter() {
         alert("Failed to set count");
         return;
       }
+
       setCount(await getCount());
+      setIncrementValue("");
+      setIsPendingInc(false);
+    } catch (e) {
+      console.info(e instanceof Error ? e.message : String(e));
+      setIsPendingInc(false);
     }
-  
+  };
 
+  // Handles reset button
+  const handleReset = async () => {
+    try {
+    if (!provider) {
+      console.log("No provider found");
+      return BigInt(0);
+    }
 
+    setIsPendingRes(true);
 
-// // event poller
-// let stop = false;
+    const op = await provider.callSC({
+      func: "reset",
+      target: CONTRACT_ADDRESS,
+    });
 
-// // Callback function for handling incoming events
-// const onData = async (events: SCEvent[]) => {
-//   for (const event of events) {
-//     console.log(
-//       `Event period: ${event.context.slot.period} thread: ${event.context.slot.thread} -`,
-//       event.data,
-//     );
-//     // setEvents(event.data);
-//   }
-// };
+    const status = await op.waitSpeculativeExecution();
 
-// // Callback function for handling errors
-// const onError = (error: Error) => {
-//   console.error('Error:', error);
-//   stop = true; // Stop polling in case of an error
-// };
+    if (status !== OperationStatus.SpeculativeSuccess) {
+      alert("Failed to set count");
+      return;
+    }
+    setCount(await getCount());
+    setIsPendingRes(false);
+  } catch (e) {
+    console.info(e instanceof Error ? e.message : String(e));
+    setIsPendingRes(false);
+  }
+  };
 
-// if (!provider) {
-//    console.error("Provider not initialized");
-//    return;
-//  }
-
-// // Start the event poller with a 5-second interval
-// const { stopPolling } = EventPoller.start(
-//   provider,
-//   {
-//     smartContractAddress: CONTRACT_ADDRESS, 
-//   },
-//   onData,
-//   onError,
-//   5000, // Polling interval in milliseconds
-// );
-
-// // Continue polling until stopped
-// while (!stop) {
-//   await scheduler.wait(5000);
-// }
-// stopPolling(); // Stop polling once the loop terminates
-
-
-
-
-
-  
-  // If no provider, displays a message to inform the user 
+  // If no provider, displays a message to inform the user
   if (!provider) {
     return (
       <div className="app-container">
@@ -204,13 +213,25 @@ export default function IncrementCounter() {
     );
   }
 
-  // If no wallet, displays a message to inform the user 
+  // If no wallet, displays a message to inform the user
   if (!connected) {
     return (
       <div className="app-container">
         <p>Wallet not connected... </p>
         <p>Please connect your Massa wallet</p>
-        <button onClick={() => connectWallet()} style={{ padding: "5px 10px", backgroundColor: "black", color: "white", borderRadius:".7em", cursor:"pointer", marginBottom: "2em"}}>Connect</button>
+        <button
+          onClick={() => connectWallet()}
+          style={{
+            padding: "5px 10px",
+            backgroundColor: "black",
+            color: "white",
+            borderRadius: ".7em",
+            cursor: "pointer",
+            marginBottom: "2em",
+          }}
+        >
+          Connect
+        </button>
       </div>
     );
   }
@@ -226,22 +247,40 @@ export default function IncrementCounter() {
           placeholder="Enter number"
           style={{ marginRight: "10px", padding: "5px" }}
         />
-        <button type="submit" style={{ padding: "5px 10px", backgroundColor: "black", color: "white", borderRadius:".7em", cursor:"pointer"}}>
+        <button
+          type="submit"
+          style={{
+            padding: "5px 10px",
+            backgroundColor: "black",
+            color: "white",
+            borderRadius: ".7em",
+            cursor: "pointer",
+          }}
+        >
           Increment
-          {isPendingInc && <span style={{marginLeft: "1em"}} className="loading loading-spinner loading-xs"></span>}
+          {isPendingInc && <span style={{ marginLeft: "1em" }} className="loading loading-spinner loading-xs"></span>}
         </button>
       </form>
       <div>
         <p>Count: {count}</p>
-        <button onClick={handleReset} type="button" style={{ padding: "5px 10px", backgroundColor: "black", color: "white", borderRadius:".7em", cursor:"pointer", marginBottom: "2em"}}>
+        <button
+          onClick={handleReset}
+          type="button"
+          style={{
+            padding: "5px 10px",
+            backgroundColor: "black",
+            color: "white",
+            borderRadius: ".7em",
+            cursor: "pointer",
+            marginBottom: "2em",
+          }}
+        >
           Reset
+          {isPendingRes && <span style={{ marginLeft: "1em" }} className="loading loading-spinner loading-xs"></span>}
         </button>
       </div>
-      <div id="userAccount">
-        account: {account}
-      </div>
-      <div>
-      </div>
+      <div id="userAccount">account: {account}</div>
+      <div></div>
     </div>
   );
 }
