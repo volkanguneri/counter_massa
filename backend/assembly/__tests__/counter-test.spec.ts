1fb9ceebad2517ae28ad7fb1bf9e3fa11ea7c08d
volkanguneri <vocci@hotmail.com>
2024-11-25 12:03:06 +0100

    refacto

--- backend/assembly/__tests__/counter-test.spec.ts
+++ backend/assembly/__tests__/counter-test.spec.ts
@@ -1,7 +1,7 @@
 import { Args, stringToBytes, u64ToBytes, boolToByte } from '@massalabs/as-types';
-import { Storage, setDeployContext, generateEvent } from '@massalabs/massa-as-sdk';
+import { Storage, setDeployContext } from '@massalabs/massa-as-sdk';
 import { increment, getCount, constructor, setCount, reset } from '../contracts/counter';
-import { setOwner, ownerAddress, isOwner } from '../contracts/utils/ownership';
+import { ownerAddress, isOwner } from '../contracts/utils/ownership';
 
 const owner: string = 'AU1JC5Q7c6mV7TcEtj2yA1Cv49JSiFz4xS8dnbGbmKHbVfrmFHvJ';
 let ownerArg: StaticArray<u8> = new Args().add(owner).serialize();
@@ -34,14 +34,12 @@ describe('Counter unit tests', () => {
     });
 
     test('increment should increment the count', () => {
-      setDeployContext(owner);
       setCount(u64ToBytes(3));
       increment(u64ToBytes(4));
       expect(getCount()).toStrictEqual(u64ToBytes(7));
     });
 
     test('reset should reset the count to 0', () => {
-      setDeployContext(owner);
       reset();
       expect(getCount()).toStrictEqual(u64ToBytes(0));
     });
@@ -55,19 +53,21 @@ describe('Counter unit tests', () => {
     test('isOwner', () =>
       expect(isOwner(ownerArg)).toStrictEqual(boolToByte(true)));
 
-    test('reset should fail if caller is not the owner', () => {
+    test('reset should fail if caller is NOT the owner', () => {
       setDeployContext(randomUser);
       expect(() => {
         reset();
       }).toThrow('Caller is not the owner');
     });
 
+    // Doesn't work if `setDeployContext(owner)` line commented !
+    // But it should work because `constructor(ownerArg)` in `beforeAll`.
     test('reset should work if caller is the owner', () => {
       setDeployContext(owner);
       expect(() => {
         reset();
       }).not.toThrow();
-      
+
     });
   });
 });
